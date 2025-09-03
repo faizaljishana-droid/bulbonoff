15ff1265e77a3d0ac62d15d34f498fcc82d5deeb
faizaljishana-droid <faizaljishana@gmail.com>
2025-09-03 10:19:10 -0700

    Add files via upload

---
+++ main - Copy.js
@@ -0,0 +1,6 @@
+function button1(){
+    document.getElementById("bulb").innerHTML='<img src="bulbonji.png">';
+}
+function button2(){
+    document.getElementById("bulb").innerHTML='<img src="bulboffji.png">';
+}
